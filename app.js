f64ce153a5463678f377e81efa98d94b117689cc
Francisco Israel Mecillas <francisco_polisol@hotmail.com>
2022-05-02 18:24:27 -0500

    4. trabajando con archivo test para prueba de unidad

--- app.js
+++ app.js
@@ -11,15 +11,28 @@ const port = 3000 // localhost:3000
 //ESTO ES UN ENDPOINT
 //routes
 //Methods HTTP:GET POST,PUT, DELETE
-app.get('/v1/explorers',(req,res) => {
-    console.log(`GET Explorers V1 API ${new Date()}`)
-    const explorer1 = {id:1, name: "Explorer1"}
-    const explorer2 = {id:2, name: "Explorer2"}
-    const explorer3 = {id:3, name: "Explorer3"}
-    const explorers = [explorer1,explorer2,explorer3]
-    //HTTP CODE STATUS: 200
-    res.status(200).json(explorers)
+// app.get('/v1/explorer',(req,res) => {
+//     console.log(`GET Explorers V1 API ${new Date()}`)
+//     const explorer1 = {id:1, name: "Explorer1"}
+//     const explorer2 = {id:2, name: "Explorer2"}
+//     const explorer3 = {id:3, name: "Explorer3"}
+//     const explorers = [explorer1,explorer2,explorer3]
+//     //HTTP CODE STATUS: 200
+//     res.status(200).json(explorer)
 
+// })
+
+app.get('/v1/explorers/:id',(req,res) => {
+    console.log(`GET BY ID Explorers V1 API ${new Date()}`)
+    const explorer1 = {id:1, name: "Explorer1"}
+    res.status(200).json(explorers) 
+})
+
+app.post('/v1/explorers/:id',(req,res) => {
+    console.log(`POST Explorer V1 API ${new Date()}`)
+    // Agregar la lógica para persistir 
+    console.log(req.body) //parámetros del request
+    res.status(201).json({message: "Creando exitosamente"})
 })
 
 
