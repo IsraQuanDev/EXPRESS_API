638a7fc12ba524431944a6ff85cba25e22e3a8c9
Francisco Israel Mecillas <francisco_polisol@hotmail.com>
2022-05-03 14:35:23 -0500

    1. Añadiendo archivo app.js

--- app.js
+++ app.js
@@ -35,6 +35,21 @@ app.post('/v1/explorers/:id',(req,res) => {
     res.status(201).json({message: "Creando exitosamente"})
 })
 
+app.put('/v1/explorers/:id',(req,res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body //Parametros de un client
+    res.status(200).json(explorers) 
+})
+
+
+app.delete('/v1/explorers/:id',(req,res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body //Parametros de un client
+    res.status(200).json({message: "Deleted "}) 
+})
+
 
 //Con esto inicializamos esta app
 app.listen(port,()=>{
